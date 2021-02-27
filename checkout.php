9a0563f99692ae415101af91d6704edd54f02b22
Ruchita Trivedi <75458159+ruchi8593@users.noreply.github.com>
2021-02-26 22:35:24 -0500

    Navigation and footer added to contact, movies and checkout

---
+++ checkout.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name = "viewport" content = "width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name = "author" content = "Prachi Bansal" />
+<meta name = "keywords" content = "It services provider, WebTree, Web services Provider, It company">
+<meta name = "description" content = "WebTree is an IT company which pledges to provide the best IT services which will help you to expand your business and target your customers. ">
+<link rel="icon" href="images/main-logo.jpg" type="image/x-icon"> 
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+<!-- FontAwesome CSS -->
+<link rel="stylesheet" href="fontawesome/css/fontawesome.min.css">
+<link rel="stylesheet" href="css/style.css">
+<title>WebTree - Web Services Company</title>
+</head>
+<body>
+<div class="container-fluid px-0 bg-grey">
+<!-- Header -->
+  <header class="theme-bg-black" role="banner">
+    <div class="container py-3">
+      <div class="row top-header">
+        <div class="col-6 social-icons"> <i class="fab fa-facebook-f mr-2"></i> <i class="fab fa-google-plus-g mr-2"></i> <i class="fab fa-twitter mr-2"></i> </div>
+        <div class="col-6"><a href="contact.html"><span class="float-right">Contact Us</span></a></div>
+      </div>
+    </div>
+  </header>
+<!-- Main Nav -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-white" id="main_nav" role="navigation">
+    <div class="container"> 
+		<a class="navbar-brand" href="index.html">
+			<img src="images/main-logo.jpg" class="img-fluid" alt="Logo" />
+		</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"> 
+		  <span class="navbar-toggler-icon"></span> 
+	  </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+			<li class="nav-item active"> <a class="nav-link" href="index.html">HOME</a><div class="underline"></div> </li>
+            <li class="nav-item"> <a class="nav-link" href="movies.php">MOVIES</a><div class="underline"></div> </li>
+            <li class="nav-item"> <a class="nav-link" href="checkout.php">CHECK OUT</a><div class="underline"></div> </li>
+		</ul>
+      </div>
+    </div>
+  </nav>
+	  
+<!-- Footer -->	
+  <footer class="bg-dark text-white" role="contentinfo">
+    
+	
+	  <div class="container py-md-4 text-center">
+		  <div class="row">
+			  <div class="col-md-12 grey-font">
+	  	<i class="fab fa-facebook-f mr-2"></i>
+		<i class="fab fa-google-plus-g mr-2"></i> 
+		<i class="fab fa-twitter mr-2"></i>
+				  </div>
+			  <div class="col-md-12">
+		<h6>&copy; 2020 | All Rights Reserved.</h6>
+				  </div>
+			  </div>
+	  </div>
+  </footer>
+</div>
+
+<!-- Optional JavaScript --> 
+<script src="js/jquery-3.4.1.slim.min.js"></script> 
+<script src="bootstrap/js/popper.min.js"></script> 
+<script src="bootstrap/js/bootstrap.min.js"></script> 
+<script src="https://kit.fontawesome.com/2b63d8a8a8.js" crossorigin="anonymous"></script>
+</body>
+</html>
